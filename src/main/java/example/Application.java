f9fcffa5c74116b316770845b8d9d7bbb2d063c9
Dramil Dodeja <dodeja_dramil@hotmail.com>
2020-04-29 19:26:28 -0700

    Restructure and Cleanup

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -1,15 +1,26 @@
-package example.smallest;
+package example;
 
+import java.util.Arrays;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
-@ComponentScan
-@EnableAutoConfiguration
-public class Application extends SpringBootServletInitializer {
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
-    public static void main(String[] args) {
-    	 SpringApplication.run(Application.class, args);
-    }
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
+	}
 }
--- src/main/java/example/controllers/WelcomeController.java
+++ src/main/java/example/controllers/WelcomeController.java
@@ -1,15 +1,13 @@
 package example.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class WelcomeController {
 	
-	@RequestMapping(method = RequestMethod.GET, produces = {"application/json"})
-	public @ResponseBody String helloWorld() {
+	@RequestMapping("/")
+	public String index() {
 		return "Greeting From Dramil Dodeja";
 	}
 }
